0847bcbc47d1877ec5c9a243c2c6d17245f6f486
Mikhail <mmteams91java@gmail.com>
2016-07-07 06:49:07 +0300

    fix UrlValidator

--- app/src/main/java/com/softdesign/devintensive/utils/Validators/UrlValidator.java
+++ app/src/main/java/com/softdesign/devintensive/utils/Validators/UrlValidator.java
@@ -19,16 +19,17 @@ public class UrlValidator extends BaseValidator{
         return mEditText.length() > mDefaultValue.length() + 2;
     }
 
-
     @Override
-    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
         if(charSequence.toString().contains(mDefaultValue)) mBefore =charSequence.toString().substring(charSequence.toString()
                 .indexOf(mDefaultValue)+mDefaultValue.length());
         else mBefore ="";
         if(i<mDefaultValue.length())mPositionCursor=mDefaultValue.length();
         else mPositionCursor=i+i1;
-//        Log.e(ConstantManager.TAG_PREFIX,i+"  curs  "+ mBefore);
+    }
 
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
     }
 
