5af52c5cd13acaf483f74b51c5358ec2464a6734
Mikhail <mmteams91java@gmail.com>
2016-07-14 10:05:00 +0300

    finished all from video + intent repo

--- app/src/main/java/com/softdesign/devintensive/ui/activities/ProfileuserActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/ProfileuserActivity.java
@@ -1,10 +1,16 @@
 package com.softdesign.devintensive.ui.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -38,6 +44,8 @@ public class ProfileUserActivity extends AppCompatActivity {
     ListView mRepoListView;
     @BindView(R.id.collapsing_toolbar)
     CollapsingToolbarLayout mCollapsingToolbar;
+    @BindView(R.id.main_coordinator_container)
+    CoordinatorLayout mCoordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +69,21 @@ public class ProfileUserActivity extends AppCompatActivity {
         final List<String> repositories = userDTO.getRepositories();
         final RepositoriesAdapter repositoriesAdapter = new RepositoriesAdapter(this, repositories);
         mRepoListView.setAdapter(repositoriesAdapter);
+
+        mRepoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Snackbar.make(mCollapsingToolbar, "Репозиторий " +repositories.get(i),Snackbar.LENGTH_LONG).show();
+                String forUri = repositories.get(i);
+                if(forUri.contains("https://"))
+                    forUri=forUri.substring(forUri.indexOf("https://")+"https://".length());
+                if(forUri.contains("http://"))
+                    forUri=forUri.substring(forUri.indexOf("http://")+"http://".length());
+                Uri uri=Uri.parse(ConstantManager.INTENT_SCHEME[3]+forUri);
+                Intent showRepo = new Intent(Intent.ACTION_DEFAULT,uri);
+                startActivity(showRepo);
+            }
+        });
         mUserBio.setText(userDTO.getBio());
         mUserRating.setText(userDTO.getRating());
         mUserCodeLines.setText(userDTO.getCodeLines());
